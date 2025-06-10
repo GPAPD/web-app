493c96d98c3435b479880dd9e4948fd0e82a7d01
akash <akash@AKASH>
2025-06-10 22:27:24 +0530

    add dynamic home page banner and add login controller

---
+++ web-app/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace web_app.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
