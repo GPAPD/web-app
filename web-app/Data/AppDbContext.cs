9a6689a750f1215762208f7ebf325e2a840729b2
akash <akash@AKASH>
2025-06-18 19:03:07 +0530

    add product table

---
+++ web-app/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using web_app.Data.Entity;
+
+namespace web_app.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> option) : base(option)
+        {
+
+        }
+        public DbSet<Product> Products{get; set;}
+    }
+}
---
+++ web-app/Data/Entity/Product.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace web_app.Data.Entity
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        public string? ProductName { get; set; }
+
+        public string? ProductDesc { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string? Category { get; set; }
+
+        public int Stock { get; set; }
+
+        public bool IsLive { get; set; }
+
+        public string? Image { get; set; }
+
+    }
+}
